37b0772a6b95808fecdc8509ce7bdbea46452022
chipcats <486626314@qq.com>
2024-06-09 16:23:36 +0800

    新增会员列表，订单列表等数据展示

--- src/views/modules/product/baseattr.vue
+++ src/views/modules/product/baseattr.vue
@@ -12,9 +12,8 @@
           <el-form-item>
             <el-button @click="getDataList()">查询</el-button>
             <el-button type="success" @click="getAllDataList()">查询全部</el-button>
-            <el-button v-if="isAuth('product:attr:save')" type="primary"
-              @click="addOrUpdateHandle()">新增</el-button>
-            <el-button v-if="isAuth('product:attr:delete')" type="danger" @click="deleteHandle()"
+            <el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
+            <el-button type="danger" @click="deleteHandle()"
               :disabled="dataListSelections.length <= 0">批量删除</el-button>
           </el-form-item>
         </el-form>
